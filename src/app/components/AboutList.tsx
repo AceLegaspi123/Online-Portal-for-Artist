a27171b71fe472ef18e814f3737b53ab794e0200
CHRISTIANJAYY\chris <christianfromfb08@gmail.com>
2025-10-03 06:42:12 +0800

    Add about section

---
+++ src/app/components/AboutList.tsx
@@ -0,0 +1,14 @@
+const AboutList = () => {
+  return (
+    <ul className="flex flex-col gap-8 text-lg opacity-70">
+        <li>Basic Information</li>
+        <li>Address</li>
+        <li>Contact</li>
+        <li>About me </li>
+        <li>Link Accounts</li>
+        <li>Skills / Creative Tags</li>
+    </ul>
+  )
+}
+
+export default AboutList;
