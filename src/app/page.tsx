003a1c96b9d542cdf91a0bf2fd826e258061254c
Niño Bongay <oninbongay03@gmail.com>
2025-10-02 21:20:19 +0800

    Added login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Footer from "./components/Footer"
 import ArtList from "./components/ArtList";
 
-
 export default function Home() {
   return (
   <div className="max-w-screen-2xl m-auto flex flex-col min-h-screen"> 
     <main className="flex justify-between items-start gap-10 flex-grow">
-      <h1>Featured artworks</h1>
       <ArtList />
       <ArtList />
       <ArtList />
