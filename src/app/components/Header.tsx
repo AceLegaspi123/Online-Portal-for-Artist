4b2ef16c94fb65a8b18d0c550bed9c3ee8ed7096
Jeaven Paras <jeavenanda07@gmail.com>
2025-10-05 04:26:44 +0800

    Modified Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,28 +1,32 @@
 'use client';
-
+import { IoIosNotifications } from "react-icons/io";
 import { BsSearch } from 'react-icons/bs';
 import Logo from "./Logo";
 import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="relative border-b-1 border-b-primary-line flex w-full justify-center items-center mb-10 p-4">
-      <div className="w-[50%] flex gap-8 items-center">
+    <header className="relative border-b-1 border-b-primary-line flex w-full justify-center items-center mb-10 p-4 px-17">
+      <div className="w-[80%] flex gap-8 items-center">
       <Link href="/"><Logo/></Link>
 
-        <p><Link href="shop">Shop</Link></p>
+        <p className='border-x-2 border-primary-line px-8'><Link href="shop">Shop</Link></p>
+        <div className=" border-primary-line border-r-2 text-2xl pr-8">
+           <IoIosNotifications/>
+        </div>
+
         <div className="flex relative">
           <BsSearch className="opacity-80 text-xl absolute top-4 left-4"/>
           <input
             type="text"
             placeholder={`Search arts/artist in ArtistryHub`}
-            className="bg-secondary opacity-80 font-normal py-4 pl-14 w-[21rem] rounded-full"
+            className="bg-secondary opacity-80 font-normal py-4 pl-14 w-[25rem] rounded-md"
           />
         </div>
 
       </div>
 
-      <div className="w-[50%] text-right flex justify-end gap-10">
+      <div className="w-[20%] text-right flex justify-end gap-10">
         <button className="cursor-pointer"><Link href="/register">Sign up</Link></button>
         <button className="bg-gradient-primary cursor-pointer text-white px-10 py-2 rounded-full "><Link href="/login">Log in</Link></button>
       </div>
