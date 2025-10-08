67a545fb891d81866dd92844fa126ee9eec2e7f3
Jeaven Paras <jeavenanda07@gmail.com>
2025-10-08 12:45:29 +0800

    edit

---
+++ src/app/components/ArtTypeCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArtTypeCard = () => {
+  return (
+    <div className='bg-primary border-2 border-primary-line px-4 py-2 rounded-sm'>
+      Digital Art
+    </div>
+  )
+}
+
+export default ArtTypeCard
