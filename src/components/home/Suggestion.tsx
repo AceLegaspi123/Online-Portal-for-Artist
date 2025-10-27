7054e2abc899164a9d2af7895d09f91289c3c314
Jeaven Paras <jeavenanda07@gmail.com>
2025-10-28 07:20:50 +0800

    Change home layout

--- src/components/home/Suggestion.tsx
+++ src/components/home/Suggestion.tsx
@@ -1,20 +1,43 @@
-import SuggestionList from "./SuggestionList"
+import classNames from "clsx";
+
+const suggestion: string[] = [
+  "All",
+  "Traditional",
+  "Modern",
+  "Spicy",
+  "Sweet",
+  "Savory",
+  "Quick & Easy",
+  "Healthy",
+  "Desserts",
+  "Dinner",
+];
 
 export default function Suggestion() {
-    return(
+  const mapList = suggestion.map((item, index) => {
+    return console.log(index + ". " + item);
+  });
+
+  console.log(mapList);
+  return (
     <div>
-        <div className="flex gap-4 justify-start items-start mt-3 ">
-            <SuggestionList />
-            <SuggestionList />
-            <SuggestionList />
-            <SuggestionList />
-            <SuggestionList />
-            <SuggestionList />
-            <SuggestionList />
-            <SuggestionList />
-            <SuggestionList />
-            <SuggestionList />
-        </div>
+      <div className="flex gap-4 justify-start items-start mt-3 w-full ">
+        <ul className="flex justify-between w-full gap-4">
+          {suggestion.map((list, i) => (
+            <li
+              className={classNames(
+                "text-center border-1 border-primary-line bg-primary rounded-sm py-2 justify-center mx-auto w-full text-sm",
+                {
+                  "border-2 border-gradient-primary": list == "All",
+                }
+              )}
+              key={i}
+            >
+              {list}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    )
+  );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 import { useState } from "react";
 import clsx from 'clsx';
 import ProfileMenu from '../Menu/ProfileMenu';
+import { BsSearch } from 'react-icons/bs';
 
 export default function Header() {
   const [isLogin, setIsLogin] = useState(true);
@@ -24,9 +25,18 @@ export default function Header() {
 
   console.log(isLogin);
   return (
-    <header className="z-20 bg-background sticky top-0 h-20 left-0 w-full border-b-1 border-b-primary-line flex justify-between items-center mb-10 p-4 px-17">
+    <header className="z-20 bg-background sticky top-0 h-20 left-0 w-full border-b-1 border-b-primary-line flex justify-between items-center mb-10 px-5 md:px-10 lg:px-20">
       <div className="flex gap-8 items-center">
           <Logo/>
+
+          {/* <div className="grow flex relative">
+            <BsSearch className="opacity-80 text-xl absolute top-4 left-4"/>
+            <input
+              type="text"
+              placeholder={`Search arts/artist in ArtistryHub`}
+              className="border-1 border-primary-line bg-secondary opacity-80 font-normal py-4 pl-14  w-90 rounded-full active:outline-0 "
+            />
+          </div>  */}
       </div>
 
       <div className="flex items-center gap-4">
