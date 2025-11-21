0fc1d141d597bdf4b96a2bd0e0fbc8c267d49b52
April Joy M. Arcamo <ajarcamo6@gmail.com>
2025-11-21 20:08:28 +0800

    asd

--- src/app/(public)/profile/page.tsx
+++ src/app/(public)/profile/page.tsx
@@ -1,8 +1,14 @@
 import { TfiGallery } from "react-icons/tfi";
+import { HiOutlineCake } from "react-icons/hi2";
+import { CiLocationOn } from "react-icons/ci";
+import { CgProfile } from "react-icons/cg";
+import { SlActionRedo } from "react-icons/sl";
+import { RiBallPenLine } from "react-icons/ri";
 
 const page = () => {
   return (
-    <div>
+    <div className="flex">
+     <div>
       <div className='p-10'>
         <h4>
           Featured Gallery
@@ -52,6 +58,44 @@ const page = () => {
       </div>
 
     </div>
+      <div><h2>About ChristianJay69</h2>
+        <div className="bg-primary h-135 w-130 mt-14 rounded-sm "> 
+          <div className="flex gap-2 m-2 p-5">
+            <HiOutlineCake className=" h-10 w-10" />
+            <div><h2>Birthday: MM/DD/YYYY</h2></div>
+          </div>
+          <div className="flex align-middle gap-2 m-2 p-5">
+            <CiLocationOn className="h-10 w-10"/>
+            <div><h2>Location: Philippines</h2></div>
+          </div>
+          <div className="flex gap-2 m-2 p-5">
+            <CgProfile className="h-10 w-10"/>
+            <div><h2>Joined : 5months ago</h2></div>
+          </div>
+          <div className="flex gap-2 m-2 p-5" >
+            <SlActionRedo className="h-10 w-10"/>
+            <div><h2>Pronoun: He/Him</h2></div>
+          </div>
+          <div className="flex gap-2 m-2 p-5">
+            <RiBallPenLine className="h-10 w-10"/>
+            <div><h2>Bio: I’m a digital artist who creates vibrant <br /> character illustrations inspired by fantasy. I <br /> love bringing ideas to life through bold <br /> colors and dynamic designs.</h2></div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>
+              <div className="bg-primary h-135 w-130 mt-14 rounded-sm ">
+                <div className="flex"><img src="https://www.imdb.com/name/nm0246703/mediaviewer/rm2328975361/?ref_=nm_ph_2" alt="" /></div>
+                <div className="">Add a new comment...</div>
+              </div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
