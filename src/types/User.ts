bf203b551f657037c51fee14b9595669fd147877
Jeaven Paras <jeavenanda07@gmail.com>
2025-11-22 07:10:24 +0800

    modify names and created new type interface

--- src/types/User.ts
+++ src/types/User.ts
@@ -6,3 +6,32 @@ export interface User {
     followers: number;
     accountStatus: string
 }
+
+export interface artList {
+    id: number;
+    src: string;
+    alt: string;
+    user: string;
+    avatar: string;
+}
+
+export interface artwork  {
+  artwork_id: number;
+  user_profile_id: string;
+  cover_photo: string;
+  art_file: string;
+  artwork_title: string;
+  description: string;
+  artwork_type: 'Digital' | 'Physical';
+  tags: string[];
+  status: 'For Sale' | 'Not for Sale';
+  price: number;
+  stocks: number;
+  sold: number;
+  gallery_id: string | null;
+  likes_count: number;
+  views: number;
+  created_at: string;
+  updated_at: string;
+  artist_avatar: string;
+};
