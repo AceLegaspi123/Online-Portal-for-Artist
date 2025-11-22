5e5567197e02c5cd940dacc2b68cffcc1e87a3cf
kristylvillamena026 <kristylvillamena026@gmail.com>
2025-11-22 10:10:21 +0800

    change color and size

--- src/app/(auth)/register/profile-setup/get-started/page.tsx
+++ src/app/(auth)/register/profile-setup/get-started/page.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import Link from "next/link";
 
-  
 function GetStarted() {
   return (
    <div className='flex min-h-screen flex-col items-center justify-center p-25'>
@@ -8,30 +7,29 @@ function GetStarted() {
 
   <div>
     <div className='grid grid-cols-3 gap-3 text-center mt-10'>
-      <div className=' lex flex-col w-72 bg-[#1c1c1c]  h-50 border-gray-300 rounded-lg shadow-md p-4 '>
+      <div className=' flex border-1 flex-col w-72 bg-primary  h-50 border-primary-line rounded-lg shadow-md p-4 '>
         <div className='h-[80%]'><img src={' /discover-art.png'} alt="Discover Art" className=" m-auto h-full object-fit p-4 " /></div>
       
           <p className="mt-2 h-[20%] font-bold ">Discover Art</p>
       </div>
 
-    <div className='flex flex-col w-72 bg-[#1c1c1c]  h-50 border-gray-700 rounded-lg shadow-md p-4'>
+    <div className='flex flex-col border-1 w-72 bg-primary  h-50 border-primary-line rounded-lg shadow-md p-4'>
       <div className='h-[80%]'>
         <img src={'/sell-art.png'} alt="Sell Art" className="m-auto h-full object-contain p-4" />
       </div>
       <p className="mt-2 h-[20%] font-bold ">Sell Art</p>
     </div>
 
-    <div className='lex flex-col w-72 bg-[#1c1c1c] h-50 border-gray-700 rounded-lg shadow-md p-4 re'>
+    <div className='lex flex-col border-1 w-72 bg-primary h-50 border-primary-line rounded-lg shadow-md p-4 re'>
       <div className='h-[80%]'>
         <img src={'/buy-art.png'} alt="Buy Art" className="m-auto h-full object-contain p-4" />
       </div>
       <p className="mt-2 h-[20%] font-bold">Buy Art</p>
     </div>
-
   </div>
 
     <div className="flex justify-center mt-10">
-      <button className="bg-[#5A5A5E] border-gray-800 text-black px-18 py-2 rounded-sm " >Continue</button>
+      <button className="bg-secondary border-1 border-primary-line text-white px-18 py-2 rounded-sm " >Continue</button>
     </div>
   </div>
 </div>
