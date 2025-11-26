574756475594656561376f1cee1f0f3a5d2a4d5d
bscs_student_1 <bscs4_code_repo@hotmail.com>
2025-11-26 15:01:03 +0800

    Added Blurtext Effect for Hero section message

---
+++ src/app/components/ui/HeroSection.tsx
@@ -0,0 +1,47 @@
+import BlurText from "./BlurText";
+
+const cardSection : string[] = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoTE1jTC7pvWtoaHM2rLuTq6duGlDVqQgllw&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWPFB5QGKP_Gy0k3jvqIpKc4Haxo9cUDJNsQ&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnK4v11m_LtS8lepDLCWBLE2rH3OoORmbR73cx0jP_q02JrlBzDOzIwX1PLizhRVqwIXE&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkQfFzMVVONAVa_PsimFsXCZFKBIZmR8Otzw&s",
+  "https://i0.wp.com/magzoid.com/wp-content/uploads/2023/07/Snapinsta.app_352253755_1421927701890393_5526314807261586687_n_1080.jpg?resize=749%2C936&ssl=1",
+  "https://images.squarespace-cdn.com/content/v1/54fc8146e4b02a22841f4df7/1697499293277-U0GFDOZUTBCFI1TK7PQR/Art_of_Sam_Yang_38.jpeg"
+];
+
+const HeroSection = () => {
+  return (
+    <section className="w-full min-h-[80vh] flex flex-col gap-8 items-center justify-center">
+        <BlurText
+            text="A place to display your masterpiece."
+            delay={150}
+            animateBy="words"
+            direction="top"
+            className="mb-8 text-7xl font-lg w-[70%] mx-auto font-semibold"
+        />
+
+        <ul className="flex gap-5 justify-center">
+              {
+                cardSection.map((item, key) => (
+                  <li className="h-55 w-55 bg-primary rounded-md overflow-hidden" key={key} > 
+                    <img 
+                      src={item}             
+                      alt="" 
+                      className="h-full w-full object-cover"  
+                    />
+                  </li>
+                ))
+              }
+            </ul>
+
+        <p>Artist can display their masterpieces, and buyer can discover and buy your art</p>
+      
+        <div className="flex gap-4">
+          <button className="bg-white text-black px-8 cursor-pointer rounded-full font-semibold py-2">Join Now</button>
+          <button className=" px-8 rounded-full font-semibold py-2 cursor-pointer ">Learn More</button>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
