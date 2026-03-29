455f4f6ef8ec6cdbab610b175dc3f3d9ac9f9c39
Jeaven Paras <jeavenanda07@gmail.com>
2026-03-29 09:24:10 +0800

    Added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ReactNode } from "react";
 import Providers from "@/app/components/Providers";
+import { Inter, Archivo_Black, JetBrains_Mono } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
 
 export const metadata: Metadata = {
@@ -9,10 +10,21 @@ export const metadata: Metadata = {
   description: "Connect artists and clients through commissions",
 };
 
+const inter = Inter({ 
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+const archivo = Archivo_Black({ 
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-archivo',
+});
+
 export default function RootLayout({ children }: {children: ReactNode}) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className={`${inter.variable} antialiased ${archivo.variable} font-sans`}>
         <Providers>
           {children}
           <Toaster position="top-center" />
