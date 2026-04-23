7b969644bde465021e1e77d951a850e9d082cc17
Jeaven Paras <jeavenanda07@gmail.com>
2026-04-24 06:28:38 +0800

    Added Imagekit IO

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import Header from '../components/layout/Header';
-import Footer from '../components/layout/Footer';
+import React from "react";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+import {ImageKitProvider } from "@imagekit/next";
 
-
-const layout = ({children}: {children: React.ReactNode}) => {
+const layout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <Header />
-      
-      
-      <main className='flex-grow mt-18'>
-        {children}
-      </main>
-      
-      <div className='bg-primary mt-20'>
-        <Footer />
-      </div>
-      
-    </div>
-  )
-}
+    <ImageKitProvider urlEndpoint={process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT}>
+      <div className="flex flex-col min-h-screen">
+        <Header />
 
-export default layout
+        <main className="flex-grow mt-18">{children}</main>
+
+        <div className="bg-primary mt-20">
+          <Footer />
+        </div>
+      </div>
+    </ImageKitProvider>
+  );
+};
+
+export default layout;
